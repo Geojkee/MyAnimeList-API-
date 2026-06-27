27c5e46204c853158a564bdb709740a2ff165932
DWTD <dwtdgm@gmail.com>
2026-06-27 22:33:27 +0300

    update

--- src/main/java/com/dwtd/myanimelist/features/auth/AuthController.java
+++ src/main/java/com/dwtd/myanimelist/features/auth/AuthController.java
@@ -4,6 +4,8 @@ import com.dwtd.myanimelist.features.auth.dto.AuthResponse;
 import com.dwtd.myanimelist.features.auth.dto.LoginRequest;
 import com.dwtd.myanimelist.features.auth.dto.RegisterRequest;
 import com.dwtd.myanimelist.features.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Tag(name = "Authentification")
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "User registration")
     @PostMapping("/sign-up")
     public AuthResponse signUp(@RequestBody @Valid RegisterRequest request) {
         return authService.signUp(request);
     }
 
+    @Operation(summary = "User login")
     @PostMapping("/sign-in")
     public AuthResponse signIn(@RequestBody @Valid LoginRequest request) {
         return authService.signIn(request);
